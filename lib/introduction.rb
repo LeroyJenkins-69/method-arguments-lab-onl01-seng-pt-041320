5292810849b6f6e9c7a2d48d37905500407a1fb3
Kordell Parrish <parrishkordell@gmail.com>
2020-04-14 17:34:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end 
+
+def introduction_with_language(name, Language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
